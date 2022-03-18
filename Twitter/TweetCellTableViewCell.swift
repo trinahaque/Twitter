631213e756acfd3785dae67f57fe0c1fbf68aca0
Trina Haque <trinahaque@Trinas-MacBook-Pro.local>
2022-03-18 15:13:07 -0700

    Implemented favorite feature

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -15,18 +15,33 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     @IBOutlet weak var favButton: UIButton!
-    
     @IBOutlet weak var retweetButton: UIButton!
-    
+    var favorited: Bool = false
+    var tweetId: Int = -1
     
     @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: {(error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+                },
+                failure: {(error) in
+                    print("Unfavorite did not succeed: \(error)")
+            })
+        }
+            
+        
     }
     
     @IBAction func retweet(_ sender: Any) {
     }
     
-    var favorited: Bool = false
-    var tweetId: Int = -1
     
     func setFavorite(_ isFavorited: Bool){
         favorited = isFavorited
