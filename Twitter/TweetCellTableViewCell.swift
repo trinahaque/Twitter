1853e92ab80d0a9643529882f2def5bbe320c9b8
Trina Haque <trinahaque@Trinas-MacBook-Pro.local>
2022-03-18 18:05:05 -0700

    Implemented retweet feature

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -40,8 +40,22 @@ class TweetCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: {(error) in
+            print("Unfavorite did not succeed: \(error)")
+        })
     }
     
+    func setRetweeted(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+    }
     
     func setFavorite(_ isFavorited: Bool){
         favorited = isFavorited
